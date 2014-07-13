06d8b298281e3461029ce2c4570fad5e6e405f31
wireshark <felixguendling@gmail.com>
2014-07-13 08:35:13 +0000

    remove corecount

--- build_tools/build.sh
+++ build_tools/build.sh
@@ -5,15 +5,13 @@ ORIGINAL_DIR=`pwd`;
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 cd $DIR;
 
-CORE_COUNT=$(grep -c ^processor /proc/cpuinfo);
-
 ./prepare.sh
 
 mkdir -p build;
 cd build;
 cat ../android.toolchain.cmake;
 cmake -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake ../../botscript-server;
-make -j$CORE_COUNT botscript-server;
+make -j botscript-server;
 cd $DIR;
 cp build/libbotscript-server.so ../jni/libs/libbotscript-server.so;
 cd ..;
